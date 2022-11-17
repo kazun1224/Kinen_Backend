02c8dfe767ce1a7642061e8965e00d5a53070839
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-17 14:45:22 +0900

    認証系のapiを作成

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { Jwt } from './interface/auth.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly config: ConfigService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async signUp(dto: AuthDto) {
+    const hashed = await bcrypt.hash(dto.password, 12);
+    try {
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashedPassword: hashed,
+        },
+      });
+      return {
+        message: 'OK',
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('This email is already taken');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where : {
+        email : dto.email,
+      }
+    })
+
+    if(!user) {
+      throw new ForbiddenException('メールかパスワードが間違っています');
+    };
+
+    const isValid = await bcrypt.compare(dto.password, user.hashedPassword)
+
+    if(!isValid) {
+      throw new ForbiddenException('メールアドレスかパスワードが間違っています')
+    }
+    return this.generateJwt(user.id, user.email)
+  }
+
+  async generateJwt(userId: number, email: string): Promise<Jwt> {
+
+    const payload = {
+      sub: userId,
+      email
+    }
+    const secret = this.config.get('JWT_SECRET')
+    const token = await this.jwtService.signAsync(payload,{
+      expiresIn: '5m',
+      secret,
+    })
+
+    return {
+      accessToken: token,
+    }
+  }
+}
