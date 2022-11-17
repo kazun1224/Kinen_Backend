5a2be6c2b4251411a1a4534f5507b8ca568222e3
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-17 14:43:21 +0900

    authのdtoの作成

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+import { MinLength, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+}
