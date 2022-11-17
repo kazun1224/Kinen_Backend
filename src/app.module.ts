73e34653f625ddc53e294af1ef190e472022c6b1
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-17 14:42:00 +0900

    ConfigModuleを全体で使えるように記述

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +7,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
