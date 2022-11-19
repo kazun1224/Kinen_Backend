00ac2c0512c3a26f4584336d31f1493a679ae800
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-19 16:44:25 +0900

    CigaretteのCRUDを行うAPIの作成

--- src/cigarette/cigarette.controller.ts
+++ src/cigarette/cigarette.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Param, ParseIntPipe, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Body, Delete, HttpCode, Patch, Post } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { Cigarette } from '@prisma/client';
 import { Request } from 'express';
 import { CigaretteService } from './cigarette.service';
+import { CreateCigaretteDto } from './dto/create-cigarette.dto';
+import { UpdateCigaretteDto } from './dto/update-cigarette';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('cigarette')
@@ -15,7 +26,40 @@ export class CigaretteController {
   }
 
   @Get(':id')
-  getCigaretteById(@Req() req: Request, @Param('id', ParseIntPipe) cigaretteId:number): Promise<Cigarette>{
-    return this.cigaretteService.getCigaretteById(req.user.id ,cigaretteId);
+  getCigaretteById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) cigaretteId: number,
+  ): Promise<Cigarette> {
+    return this.cigaretteService.getCigaretteById(req.user.id, cigaretteId);
+  }
+
+  @Post()
+  createCigarette(
+    @Req() req: Request,
+    @Body() dto: CreateCigaretteDto,
+  ): Promise<Cigarette> {
+    return this.cigaretteService.createCigarette(req.user.id, dto);
+  }
+
+  @Patch(':id')
+  updateCgaretteById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) cigaretteId: number,
+    @Body() dto: UpdateCigaretteDto,
+  ) {
+    return this.cigaretteService.updateCgaretteById(
+      req.user.id,
+      cigaretteId,
+      dto,
+    );
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteCigaretteById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) cigaretteId: number,
+  ) {
+    return this.cigaretteService.deleteCigaretteById(req.user.id, cigaretteId);
   }
 }
--- src/cigarette/cigarette.service.ts
+++ src/cigarette/cigarette.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { Cigarette } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCigaretteDto } from './dto/create-cigarette.dto';
+import { UpdateCigaretteDto } from './dto/update-cigarette';
 
 @Injectable()
 export class CigaretteService {
@@ -16,12 +18,73 @@ export class CigaretteService {
       },
     });
   }
-  async getCigaretteById(userId: number, cigaretteId:number): Promise<Cigarette> {
+  async getCigaretteById(
+    userId: number,
+    cigaretteId: number,
+  ): Promise<Cigarette> {
     return this.prisma.cigarette.findFirst({
       where: {
         userId,
-        id:cigaretteId,
+        id: cigaretteId,
+      },
+    });
+  }
 
+  async createCigarette(
+    userId: number,
+    dto: CreateCigaretteDto,
+  ): Promise<Cigarette> {
+    const cigarette = await this.prisma.cigarette.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    });
+    this.prisma.cigarette.findMany;
+    return cigarette;
+  }
+
+  async updateCgaretteById(
+    userId: number,
+    cigaretteId: number,
+    dto: UpdateCigaretteDto,
+  ) {
+    const cigarette = await this.prisma.cigarette.findUnique({
+      where: {
+        id: cigaretteId,
+      },
+    });
+    if (!cigarette || cigarette.userId !== userId) {
+      throw new ForbiddenException('No permission to update');
+    }
+
+    return await this.prisma.cigarette.update({
+      where: {
+        id: cigaretteId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteCigaretteById(
+    userId: number,
+    cigaretteId: number,
+  ): Promise<void> {
+    const cigarette = await this.prisma.cigarette.findUnique({
+      where: {
+        id: cigaretteId,
+      },
+    });
+
+    if (!cigarette || cigarette.userId !== userId) {
+      throw new ForbiddenException('No permission to delete');
+    }
+
+    await this.prisma.cigarette.delete({
+      where: {
+        id: cigaretteId,
       },
     });
   }
