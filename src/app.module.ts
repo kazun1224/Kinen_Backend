a7b099b168add027df43ca1983ccae6b40bbdcaa
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-20 21:54:44 +0900

    🎨

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { CigaretteModule } from './cigarette/cigarette.module';
+import { TotalModule } from './total/total.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CigaretteModule } from './cigarette/cigarette.module';
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
     CigaretteModule,
+    TotalModule,
   ],
   controllers: [AppController],
   providers: [AppService],
