7e21b99d4192ec6ac97b2b4dfa4d4d606389d258
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-27 19:58:42 +0900

    cartonのデータをも同時に作成,更新できるように変更

--- src/cigarette/cigarette.service.ts
+++ src/cigarette/cigarette.service.ts
@@ -1,15 +1,12 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { Cigarette } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { TotalService } from 'src/total/total.service';
 import { CreateCigaretteDto } from './dto/create-cigarette.dto';
 import { UpdateCigaretteDto } from './dto/update-cigarette';
 
 @Injectable()
 export class CigaretteService {
-  constructor(
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   getCigarettes(userId: number): Promise<Cigarette[]> {
     return this.prisma.cigarette.findMany({
@@ -34,41 +31,33 @@ export class CigaretteService {
     });
   }
 
-  async createCigarette(
-    userId: number,
-    dto: CreateCigaretteDto,
-  ): Promise<Cigarette> {
-    const cigarette = await this.prisma.cigarette.create({
+  async createCigarette(userId: number, dto: CreateCigaretteDto) {
+    const cigaretteDto = {
+      name: dto.name,
+      tar: dto.tar,
+      numberOfCigarette: dto.numberOfCigarette,
+      amount: dto.amount,
+    };
+    const cartonDto = {
+      boxes: dto.boxes,
+      cartonAmount: dto.cartonAmount,
+    };
+    const newCigarette = await this.prisma.cigarette.create({
       data: {
         userId,
-        ...dto,
+        ...cigaretteDto,
+        carton: {
+          create: {
+            ...cartonDto,
+          },
+        },
+      },
+      include: {
+        carton: true,
       },
     });
 
-    // const cigarettes = await this.prisma.cigarette.findMany({
-    //   where: {
-    //     userId,
-    //   },
-    //   select: {
-    //     amount: true,
-    //   },
-    // });
-    // const amounts = cigarettes.map((cigarette) => cigarette.amount);
-
-    // const totalAmount = amounts.reduce((total, amount) => {
-    //   return total + amount;
-    // });
-
-    // await this.prisma.total.create({
-    //   data: {
-    //     userId,
-    //     totalAmount,
-    //   }
-    // })
-
-
-
-    return cigarette;
+    return newCigarette;
   }
 
   async updateCigaretteById(
@@ -76,23 +65,65 @@ export class CigaretteService {
     cigaretteId: number,
     dto: UpdateCigaretteDto,
   ) {
-    const cigarette = await this.prisma.cigarette.findUnique({
+    const cigaretteDto = {
+      name: dto.name,
+      tar: dto.tar,
+      numberOfCigarette: dto.numberOfCigarette,
+      amount: dto.amount,
+    };
+
+    const cartonDto = {
+      boxes: dto.boxes,
+      cartonAmount: dto.cartonAmount,
+    };
+
+    const prevCigarette = await this.prisma.cigarette.findUnique({
       where: {
         id: cigaretteId,
       },
     });
-    if (!cigarette || cigarette.userId !== userId) {
+    if (!prevCigarette || prevCigarette.userId !== userId) {
       throw new ForbiddenException('No permission to update');
     }
 
-    return await this.prisma.cigarette.update({
+    const prevCarton = await this.prisma.carton.findFirst({
+      where: {
+        cigaretteId: cigaretteId,
+      },
+    });
+
+    if (!prevCarton) {
+      throw new ForbiddenException('No permission to update');
+    }
+
+    const newCigarette = await this.prisma.cigarette.update({
       where: {
         id: cigaretteId,
       },
       data: {
-        ...dto,
+        ...cigaretteDto,
+        carton: {
+          update: {
+            ...cartonDto,
+          },
+        },
+      },
+      include: {
+        carton: true,
       },
     });
+    // const newCarton = await this.prisma.carton.update({
+    //   where: {
+    //     id: prevCarton.id,
+    //   },
+    //   data: {
+    //     ...cartonDto,
+    //   },
+    // });
+
+    // const createdCigaretteDate = { ...newCigarette, ...newCarton };
+
+    return newCigarette;
   }
 
   async deleteCigaretteById(
