4ec0541a234c058658a1af17a9a4b1cbc3ef7278
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-27 19:59:56 +0900

    totalのAPIを作成

--- src/total/total.controller.ts
+++ src/total/total.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { TotalService } from './total.service';
@@ -6,11 +6,18 @@ import { TotalService } from './total.service';
 @UseGuards(AuthGuard('jwt'))
 @Controller('total')
 export class TotalController {
-  constructor(private readonly totalService: TotalService) {
-
-  }
+  constructor(private readonly totalService: TotalService) {}
   @Get()
-  getTotal(@Req() req:Request) {
+  getTotal(@Req() req: Request) {
     return this.totalService.getTotal(req.user.id);
+  }
+
+  @Post('cigarette')
+  totalCalcByOnePack(@Req() req: Request,@Body('cigaretteId', ParseIntPipe) cigaretteId: number) {
+    return this.totalService.totalCalcByOnePack(req.user.id, cigaretteId);
+  }
+  @Post('carton')
+  totalCalcByCarton(@Req() req: Request, cigaretteId: number) {
+    return this.totalService.totalCalcByCarton(req.user.id, cigaretteId);
   }
 }
